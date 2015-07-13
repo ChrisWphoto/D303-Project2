4eec572bea4ee6b9a7442d854cddbffe7323705a
jwhipple07 <jwhipple07@gmail.com>
2015-07-13 11:56:01 -0500

    Update evaluator.cpp

--- D303-Project2/evaluator.cpp
+++ D303-Project2/evaluator.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <cmath>
 #include <sstream>
 #include <stack>
 #include "evaluator.h"
-																				//** KEY ***************************************
-using namespace std;															//  | : ||     & : &&      = : ==      n : !=
+using namespace std;															//** KEY ***************************************
+																				//  | : ||     & : &&      = : ==      n : !=
 																				//  g : >=     l : <=      ~ : negative
 const string evaluator::operators = "|&=n>g<l+-*/%^~di!";						//  d : --     i : ++
 const int evaluator::precedence[] = {1,2,3,3,4,4,4,4,5,5,6,6,6,7,8,8,8,8};		/////////////////////////////////////////////////
-//3-5-2*6+5*5
+
 int evaluator::exp_evaluator(const string expression)
 	{
 		bool completed = false;
 		bool digit = false;
 		char next_char, upcoming;
-		istringstream tokens(expression);
+		istringstream tokens(expression+' ');//added ending whitespace to help with any issues of missing the last digit on putback 7-13
+											//may not be necessary for ending whitespace, but it made my ++2 work.
+
 		//Read from string until empty
 		while(tokens>>next_char)
 		{ 
-			if(isdigit(next_char)){ //if it's a digit then replace and take out as int
+			if(isdigit(next_char)){ 
+				//if it's a digit then replace and take out as int
 				tokens.putback(next_char);
 				int num;
 				tokens>>num;
+
 				if (digit == false)	{ operand_stack.push(num); }
-				else { 
-					//error gott figure out how to terminate program
-					cout << "ERROR After character '" << num << "' expecting an operator, received a digit";
-					return 0;
-				}
+				else { throw Syntax_Error("Error after character");}
 				
 				digit = true; //now expecting an operator
 			}
@@ -36,17 +37,17 @@ int evaluator::exp_evaluator(const string expression)
 				//Check for next character and if its a + or - then take to correct function
 				upcoming = tokens.peek();
 				switch (upcoming){
-				case '+':
-					is_increment(tokens, next_char);
-					tokens >> next_char;
-					break;
-				case '-':
-					is_decrement(tokens, next_char);
-					tokens >> next_char;
+				case '+':case '-':
+					is_decrement_increment(tokens, next_char, upcoming);
 					break;
 				default:
+					//if operator stack is not empty then figure out precedence, 
+					//if it is empty then automatically add it
 					if (!operator_stack.empty()){
 						char top = operator_stack.top();
+
+						//if top is less precedence than next char, add to stack, 
+						//else solve the equation until next char is >= to top
 						if (Precedence(top) < Precedence(next_char)){
 							operator_stack.push(next_char);
 						}
@@ -62,7 +63,9 @@ int evaluator::exp_evaluator(const string expression)
 			}
 
 		}
-		completed = true;
+		completed = true; //finished stringstream
+
+		//finish equation until all operators are gone
 		while(!operator_stack.empty()){
 			find_equation(operator_stack.top(), completed);
 		}
@@ -75,12 +78,21 @@ void evaluator::find_equation(char oper, bool completed)
 {
 	int lhs, rhs, answer;
 	char top;
+	//if operator stack is empty throw error
+	if(operator_stack.empty())
+		throw Syntax_Error("The operator stack is empty, not enough operators to complete equation");
 	top = operator_stack.top();
 
 	while(!(Precedence(top) < Precedence(oper)))
 	{
+		//if operand stack is empty, throw error
+		if(operand_stack.empty())
+			throw Syntax_Error("The operand stack is empty, too many operators, not enough operands");
 		rhs = operand_stack.top();
 		operand_stack.pop();
+		//if operand stack is empty throw error
+		if(operand_stack.empty())
+			throw Syntax_Error("The operand stack is empty, too many operators, not enough operands");
 		lhs = operand_stack.top();
 		operand_stack.pop();
 		operator_stack.pop();
@@ -90,7 +102,7 @@ void evaluator::find_equation(char oper, bool completed)
 			{break;}
 		top = operator_stack.top();
 	}
-	if(!completed)
+	if(!completed)//add the operator back onto the stack before returning
 		operator_stack.push(oper);
 }
 
@@ -115,84 +127,72 @@ int evaluator::solve(int lhs, int rhs, char oper)
 	case '%':
 		answer = lhs % rhs;
 		break;
+	case '^':
+		answer = pow(static_cast<double>(lhs), static_cast<double>(rhs));
+		break;
 	}
 
 	return answer;
 }
 
 
-void evaluator::is_increment(istringstream& tokens, char next_char)
+void evaluator::is_decrement_increment(istringstream& tokens, char next_char, char lookfor)
 {
-	//need to verify that its not 3++2 but 3+++2 works
 	int count = 0;
-	char upcoming;
+	char original;
+	original = next_char;
+	
+	
 	do
 	{
 		operator_stack.push(next_char);
 		tokens>>next_char;
-		upcoming = tokens.peek();
 		count++;
 
-	} while (next_char == '+');
+	} while (next_char == lookfor && !(tokens.eof())); //continues until all '+' are gone, or stream is finished
+	
+	//checks that it's not 1++2 or just ++2 also can see if its format is similar to -++2
+	if(count%2 != 1 && !operand_stack.empty() && original == lookfor)
+		throw Syntax_Error("Not enough '+' operators to increment");
+
+
 	if (isdigit(next_char))
 	{
-		tokens.putback(next_char);
 		int num;
-		tokens >> num;
+		tokens.putback(next_char);
+		
+		tokens>>num;
 		operand_stack.push(num);
+		char first,second;
 		int temp;
 		for (int i = 0; i < count / 2; i++)
 		{
+			if(operator_stack.empty())
+				throw Syntax_Error("The operator stack is empty, cannot increment/decrement");
+			first = operator_stack.top();
 			operator_stack.pop();
+			if(operator_stack.empty())
+				throw Syntax_Error("The operator stack is empty, cannot increment/decrement");
+			second = operator_stack.top();
 			operator_stack.pop();
-			//Probably need to check the two that were popped to see if they were both '+'
-
+			//check the two that were popped to see if they were both '-'
+			if(first != lookfor || second != lookfor)
+				throw Syntax_Error("Expected '++', did not receive it");
+			if(operand_stack.empty())
+				throw Syntax_Error("The operand stack is empty, cannot increment/decrement without a digit!");
 			temp = operand_stack.top();
 			operand_stack.pop();
-			temp++;
+			if(lookfor == '+')
+				temp++;
+			else
+				temp--;
 			operand_stack.push(temp);
 		}
 	}
 	else
 	{
-		cout << "ERROR!";
-		system("pause");
+		throw Syntax_Error("Expected a digit, did not receive it");
 	}
-}
-void evaluator::is_decrement(istringstream& tokens, char next_char)
-{
-	//not finished
-	int count = 0;
-	char upcoming = tokens.peek();
-
-	do
-	{
-		operator_stack.push(next_char);
-		tokens >> next_char;
-		upcoming = tokens.peek();
-		count++;
-
-	} while (next_char == '-');
-	if (isdigit(next_char))
-	{
-		tokens.putback(next_char);
-		int num;
-		tokens >> num;
-		operand_stack.push(num);
-		int temp;
-		for (int i = 0; i < count / 2; i++)
-		{
-			operator_stack.pop();
-			operator_stack.pop();
-			//Probably need to check the two that were popped to see if they were both '+'
-
-			temp = operand_stack.top();
-			operand_stack.pop();
-			temp--;
-			operand_stack.push(temp);
-		}
-	}
-
 }
 void evaluator::is_bool(istringstream& is, char next_char)
 {
